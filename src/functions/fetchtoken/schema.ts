f998c2f7316f0528f73800b293e0de29f7a4c74c
deepakpraka <131285971+deepakpraka@users.noreply.github.com>
2023-07-03 22:36:06 +0530

    Update schema.ts

--- src/functions/fetchtoken/schema.ts
+++ src/functions/fetchtoken/schema.ts
@@ -6,7 +6,7 @@ export default {
       type: 'object',
       properties: {
         chainId: { type: 'string', enum: ['0', '1', '2', '3', '4', '5', '6','8'] },
-        chainId: { type: 'string', enum: ['0', '1', '2', '3', '4', '5', '6'] },
+        chainId: { type: 'string', enum: ['0', '1', '2', '3', '4', '5', '6','7'] },
       },
       additionalProperties: false,
     },
