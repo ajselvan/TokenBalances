1d91ec4ccde84b173f5aae640a88b6ea17ae027a
deepakpraka <131285971+deepakpraka@users.noreply.github.com>
2023-07-03 22:29:37 +0530

    Update schema.ts

--- src/functions/fetchtoken/schema.ts
+++ src/functions/fetchtoken/schema.ts
@@ -5,11 +5,8 @@ export default {
     queryStringParameters: {
       type: 'object',
       properties: {
-<<<<<<< HEAD
         chainId: { type: 'string', enum: ['0', '1', '2', '3', '4', '5', '6','8'] },
-=======
         chainId: { type: 'string', enum: ['0', '1', '2', '3', '4', '5', '6'] },
->>>>>>> origin/develop
       },
       additionalProperties: false,
     },
