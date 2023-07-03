d0039fd50678c3b03d1bece115d0b7d2e65ac7d1
deepakpraka <131285971+deepakpraka@users.noreply.github.com>
2023-07-03 22:30:44 +0530

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,7 +1,4 @@
 export { default as hello } from './hello';
 export {default as fetchtoken} from './fetchtoken'
-<<<<<<< HEAD
 export {default as storedetails} from './storedetails'
-=======
 export { default as tokenbalance } from './Token Balance'
->>>>>>> origin/develop
